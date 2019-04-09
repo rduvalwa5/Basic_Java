2dddf54e6b3c2e56e35b04fd293b09dc421c0959
rduvalwa2 <rduvalwa2@c1246895-osx.home>
2019-04-09 00:03:59 -0700

    adding java files code

---
+++ JavaFiles/CreateFile.java
@@ -0,0 +1,25 @@
+package Files;
+
+/* https://www.w3schools.com/java/java_files.asp 
+*/
+
+import java.io.File;  // Import the File class
+import java.io.IOException;  // Import the IOException class to handle errors
+
+public class CreateFile { 
+  public static void main(String[] args) { 
+	  
+    try { 
+//      File myObj = new File("filename.txt"); 
+      File myObj = new File("/Users/rduvalwa2/java-workspace/JavaFiles/src/Files/filename.txt");
+      if (myObj.createNewFile()) { 
+        System.out.println("File created: " + myObj.getName()); 
+      } else { 
+        System.out.println("File already exists."); 
+      } 
+    } catch (IOException e) {
+      System.out.println("An error occurred.");
+      e.printStackTrace(); 
+    } 
+  } 
+}
